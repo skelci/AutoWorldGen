19623e85d62584457624f68d195b66123316d955
Skelci <nikomaren2@gmail.com>
2024-12-18 13:18:00 +0100

    something's fucked my man

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import bpy
 import sys
 import math
 import time
+import threading
 import numpy as np
 import random as rnd
+import multiprocessing
 import concurrent.futures
 import NPerlinNoise as npn
 
@@ -37,7 +39,7 @@ def add_global_light():
     # Rotate the light to cast shadows appropriately
     light_object.rotation_euler = (math.radians(50), 0, math.radians(30))
 
-def create_blue_sphere(location=(0, 0, 0), radius=1):
+def create_blue_sphere(location=(0, 0, 0), radius=1, name="BlueSphere"):
     # Add a UV sphere
     bpy.ops.mesh.primitive_ico_sphere_add(radius=radius, location=location, subdivisions=2)
     sphere = bpy.context.active_object
@@ -54,6 +56,8 @@ def create_blue_sphere(location=(0, 0, 0), radius=1):
         sphere.data.materials[0] = material
     else:
         sphere.data.materials.append(material)
+    
+    sphere.name = name
 
     return sphere
 
@@ -76,7 +80,7 @@ def get_distance_map(chunk_size, world_size, origin=(0, 0)):
 
     values = np.sqrt(x_diff ** 2 + y_diff ** 2) * np.sqrt(2)
 
-    modifier_map = values.astype(np.float32)
+    modifier_map = values.astype(np.float16)
     return modifier_map
 
 def prepare_plane_data(location, size, subdivisions, height_array, biome_influences):
@@ -218,25 +222,10 @@ def create_mesh_object(name, verts, faces, colors):
     else:
         obj.data.materials.append(material)
 
-def lerp(a, b, t):
-    return a + (b - a) * t
-
 def distance(a, b = (0, 0)):
     return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
-def tetrahedron_volume(A, B, C, D):
-    AB = np.array(B) - np.array(A)
-    AC = np.array(C) - np.array(A)
-    AD = np.array(D) - np.array(A)
-    cross_product = np.cross(AC, AD)
-    dot_product = np.dot(AB, cross_product)
-    volume = abs(dot_product) / 6
-    return volume
-
 def gradient_and_height(noise_map, x, y):
-    size = noise_map.shape[0]
-    hsize = size // 2
-
     ix = int(np.floor(x))
     iy = int(np.floor(y))
     tx = x - ix
@@ -270,91 +259,200 @@ def gradient_and_height(noise_map, x, y):
     gradient = np.array([dzdx, dzdy])
     return gradient, z
 
-def simulate_erosion(noise_map_):
-    noise_map = noise_map_.copy()
+def simulate_droplet(noise_map, pos_start, params, noise_map_lock):
     size = noise_map.shape[0]
+    hsize = size // 2
+    max_steps = params['max_steps']
+    gravity = params['gravity']
+    erosion_rate = params['erosion_rate']
+    min_sediment = params['min_sediment']
+    inertia = params['inertia']
+    sediment_capacity_factor = params['sediment_capacity_factor']
+    brush_radius = params['brush_radius']
+    evaporate_speed = params['evaporate_speed']
 
-    max_steps = 100
-    droplet_count = int(size ** 2 * 1.5)
-    gravity = -0.1
-    erosion_rate = 0.1
-    min_sediment = 0.01
-    inertia = 0.25
-    sediment_capacity_factor = 4
-    brush_radius = 3
-    evaporate_speed = 0.01
+    pos = pos_start.copy()
+    velocity = 1.0
+    water = 1.0
+    sediment = 0.0
+    dir = np.array([0.0, 0.0], dtype=np.float32)
 
-    for droplet_id in range(droplet_count):
-        pos = np.array([rnd.randint(1, size - 2), rnd.randint(1, size - 2)], dtype=np.float64)
-        velocity = 1.0
-        water = 1.0
-        sediment = 0.0
-        dir = np.array([0.0, 0.0], dtype=np.float64)
+    for step in range(100):
+        print("something")
+        ix, iy = int(np.floor(pos[0])), int(np.floor(pos[1]))
+        tx, ty = pos[0] - ix, pos[1] - iy
 
-        if droplet_id % 4096 == 0:
-            print(f"Simulating droplet {droplet_id}/{droplet_count}")
+        gradient, z = gradient_and_height(noise_map, pos[0], pos[1])
 
-        for step in range(max_steps):
+        dir = (dir * inertia - gradient * (1 - inertia))
+        dir_len = distance(dir)
+        if dir_len != 0:
+            dir /= dir_len
 
-            ix, iy = int(np.floor(pos[0])), int(np.floor(pos[1]))
-            tx, ty = pos[0] - ix, pos[1] - iy
+        pos += dir
 
-            # Get the gradient and height at the current position
-            gradient, z = gradient_and_height(noise_map, pos[0], pos[1])
+        if pos[0] < 0 or pos[0] >= size - 1 or pos[1] < 0 or pos[1] >= size - 1 or dir_len < 0.001:
+            print("Droplet out of bounds")
+            break
 
-            # Update the direction
-            dir = (dir * inertia - gradient * (1 - inertia))
-            dir_len = distance(dir)
-            if dir_len != 0:
-                dir /= dir_len
+        _, new_z = gradient_and_height(noise_map, pos[0], pos[1])
+        delta_z = new_z - z
 
-            pos += dir
+        sediment_capacity = max(-delta_z * velocity * water * sediment_capacity_factor, min_sediment)
 
-            # Check if the position is out of bounds or if the direction is too small
-            if pos[0] < 0 or pos[0] > size - 1 or pos[1] < 0 or pos[1] > size - 1 or dir_len < 0.001:
-                break
+        if sediment > sediment_capacity or delta_z > 0:
+            for_deposit = min(sediment, delta_z) if delta_z > 0 else (sediment - sediment_capacity) * erosion_rate
+            sediment -= for_deposit
 
-            # Get the new gradient and height at the new position
-            _, new_z = gradient_and_height(noise_map, pos[0], pos[1])
-            delta_z = new_z - z
-
-            # calculate the sediment capacity
-            sediment_capacity = max(-delta_z * velocity * water * sediment_capacity_factor, min_sediment)
-
-            # If we have more sediment than the capacity or flow is uphill
-            if sediment > sediment_capacity or delta_z > 0:
-                for_deposit = min(sediment, delta_z) if delta_z > 0 else (sediment - sediment_capacity) * erosion_rate
-                sediment -= for_deposit
-
+            with noise_map_lock:
                 noise_map[iy, ix] += for_deposit * (1 - tx) * (1 - ty)
                 noise_map[iy, ix + 1] += for_deposit * tx * (1 - ty)
                 noise_map[iy + 1, ix] += for_deposit * (1 - tx) * ty
                 noise_map[iy + 1, ix + 1] += for_deposit * tx * ty
-            else:
-                for_erode = min((sediment_capacity - sediment) * erosion_rate, -delta_z)
+        else:
+            for_erode = min((sediment_capacity - sediment) * erosion_rate, -delta_z)
 
-                brush_area = get_distance_map(brush_radius, 1, origin=(tx, ty))
-                brush_area = -np.minimum(1, brush_area)
-                brush_area /= np.sum(brush_area)
-                brush_area = brush_area[:brush_radius, :brush_radius]
+            brush_area = get_distance_map(brush_radius, 1, origin=(tx, ty))
+            brush_area = -np.minimum(1, brush_area)
+            brush_area /= np.sum(brush_area)
 
-                if iy - brush_radius < 0 or iy + brush_radius > size - 1 or ix - brush_radius < 0 or ix + brush_radius > size - 1:
-                    break
-                noise_map[iy - 1:iy + 2, ix - 1:ix + 2] -= brush_area * for_erode
-                sediment += for_erode
+            x_start = ix - brush_radius // 2
+            y_start = iy - brush_radius // 2
 
-            velocity = math.sqrt(abs(velocity ** 2 + gravity * delta_z))
-            water *= (1 - evaporate_speed)
-
-            if velocity < 0.0001 and delta_z < 0.0001:
+            if x_start < 0 or x_start + brush_area.shape[1] > size or y_start < 0 or y_start + brush_area.shape[0] > size:
+                print("Brush out of bounds")
                 break
-    
+
+            with noise_map_lock:
+                noise_map[y_start:y_start + brush_area.shape[0], x_start:x_start + brush_area.shape[1]] += brush_area * for_erode
+
+            sediment += for_erode
+
+        velocity = math.sqrt(max(velocity ** 2 + gravity * delta_z, 0))
+        water *= (1 - evaporate_speed)
+
+        print(f"Step {step:<3}: pos=({pos[0]:>6.3f}, {pos[1]:>6.3f}), "
+        f"velocity={velocity:>5.3f}, water={water:>4.3f}, "
+        f"sediment={sediment:>5.3f}, delta_z={delta_z:>4.3f}")
+        create_blue_sphere((pos[0] - hsize, pos[1] - hsize, new_z + 0.1), 0.1, f"Droplet_{step}")
+
+        if velocity < 0.0001 and delta_z < 0.0001:
+            print("Droplet stopped")
+            break
+
+def simulate_droplet_wrapper(noise_map, pos_start, params, noise_map_lock, active_positions, active_positions_lock):
+    try:
+        simulate_droplet(noise_map, pos_start, params, noise_map_lock)
+    finally:
+        with active_positions_lock:
+            active_positions.remove(tuple(pos_start))
+
+def simulate_erosion(noise_map_):
+    noise_map = noise_map_.copy()
+    size = noise_map.shape[0]
+
+    droplets_per_m2 = 1.5
+    params = {
+        'max_steps': 100,
+        'gravity': -4,
+        'erosion_rate': 0.3,
+        'min_sediment': 0.01,
+        'inertia': 0.05,
+        'sediment_capacity_factor': 4,
+        'brush_radius': 3,
+        'evaporate_speed': 0.015
+    }
+    droplet_count = int(size ** 2 * droplets_per_m2)
+    droplet_count = 1
+    max_distance = params['max_steps'] + params['brush_radius']
+
+    # Generate all starting positions
+    start_positions = [np.array([rnd.randint(1, size - 2), rnd.randint(1, size - 2)], dtype=np.float32) for _ in range(droplet_count)]
+
+    active_positions = set()
+    active_positions_lock = threading.Lock()
+    noise_map_lock = threading.Lock()
+    max_threads = multiprocessing.cpu_count()
+
+    print(f"Simulating {droplet_count} droplets with {max_threads} threads")
+
+    t = time.time()
+    t_buffer = [10] * 10
+
+    # Initialize ThreadPoolExecutor
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        for i, pos_start in enumerate(start_positions):
+            # Print some progress info
+            if i % 1000 == 0:
+                t_now = time.time()
+                time_diff = t_now - t
+                t = t_now
+                t_buffer[(i // 1000) % 10] = time_diff
+                avg_time_per_1k = sum(t_buffer) / len(t_buffer)
+                print(f"Simulating droplet {i}/{droplet_count}. Time per 1k droplets: {avg_time_per_1k:.4f} seconds")
+
+                if i % 10000 == 0 and i > 0:
+                    remaining_batches = (droplet_count - i) // 1000
+                    remaining_time_sec = avg_time_per_1k * remaining_batches
+                    remaining_time_min = remaining_time_sec / 60
+                    remaining_hours = remaining_time_min // 60
+                    remaining_minutes = remaining_time_min % 60
+                    print(f"Remaining time: {remaining_hours:.0f} hours {remaining_minutes:.2f} minutes")
+
+            # Wait until we can start the job
+            while True:
+                with active_positions_lock:
+                    # Check if any active droplet is within max_distance
+                    can_start = all(distance(pos_start, pos) > max_distance for pos in active_positions)
+                    if can_start:
+                        active_positions.add(tuple(pos_start))
+                        break
+                time.sleep(0.0001)  # Sleep for 0.1ms
+
+            # Submit the droplet simulation to the thread pool
+            executor.submit(simulate_droplet_wrapper, noise_map, pos_start, params, noise_map_lock, active_positions, active_positions_lock)
+
     return noise_map
 
 def timeit(t, name):
     t_now = time.time()
     print(f"{name}{'_' * max(0, 48 - len(name))}: {t_now - t:.4f} seconds")
     return t_now
+
+def generate_3d_map(map, biome_maps, chunk_size, subdivisions, int_world_size):
+    t = time.time()
+
+    # Prepare chunk coordinates
+    chunk_coords = [
+        (x, y)
+        for x in range(-int_world_size, int_world_size + 1)
+        for y in range(-int_world_size, int_world_size + 1)
+        if math.ceil(math.sqrt(x ** 2 + y ** 2) - 0.75) <= int_world_size
+    ]
+    
+    t = timeit(t, "Prepare chunk coordinates")
+
+    # Function to prepare data for a chunk
+    def prepare_chunk_data(coords):
+        x, y = coords
+        location = (x * chunk_size, y * chunk_size, 0)
+        heights_chunk = get_chunk_heights(x, y, chunk_size, subdivisions, map)
+        biome_influences = get_chunk_biome_influences(x, y, chunk_size, subdivisions, biome_maps)
+        verts, faces, colors = prepare_plane_data(location, chunk_size, subdivisions, heights_chunk, biome_influences)
+        name = f"Plane_{x}_{y}"
+        return (name, verts, faces, colors)
+
+    # Prepare data in parallel
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        mesh_data_list = list(executor.map(prepare_chunk_data, chunk_coords))
+
+    t = timeit(t, "Prepare chunk data")
+
+    # Create meshes
+    for name, verts, faces, colors in mesh_data_list:
+        create_mesh_object(name, verts, faces, colors)
+    
+    t = timeit(t, "Create meshes")
 
 def create_terrain():
     t = time.time()
@@ -429,6 +527,8 @@ def create_terrain():
 
     noise_map_before = plains + hills + mountains
 
+    noise_map_before = get_distance_map(chunk_size, world_size, origin=(0, 0)) / 4
+
     t = timeit(t, "Generate biome influence maps")
 
     noise_map = simulate_erosion(noise_map_before)
@@ -436,9 +536,10 @@ def create_terrain():
     t = timeit(t, "Erosion simulation")
 
     river_modifications = noise_map_before - noise_map
-    max, min = abs(np.max(river_modifications)), abs(np.min(river_modifications))
-    river = np.clip(river_modifications / min, -1, 0)
-    sand = -np.clip(river_modifications / max, 0, 1)
+    max_val, min_val = abs(np.max(river_modifications)), abs(np.min(river_modifications))
+    min_val, max_val = max(min_val, 1e-3), max(max_val, 1e-3)
+    river = np.clip(river_modifications / min_val, -1, 0)
+    sand = -np.clip(river_modifications / max_val, 0, 1)
     
     biome_maps = {
         'plains': plains_biome,
@@ -450,37 +551,23 @@ def create_terrain():
 
     t = timeit(t, "Set biome maps and river/sand maps")
 
-    # Prepare chunk coordinates
-    chunk_coords = [
-        (x, y)
-        for x in range(-int_world_size, int_world_size + 1)
-        for y in range(-int_world_size, int_world_size + 1)
-        if math.ceil(math.sqrt(x ** 2 + y ** 2) - 0.75) <= int_world_size
-    ]
-    
-    t = timeit(t, "Prepare chunk coordinates")
+    generate_3d_map(noise_map, biome_maps, chunk_size, subdivisions, int_world_size)
 
-    # Function to prepare data for a chunk
-    def prepare_chunk_data(coords):
-        x, y = coords
-        location = (x * chunk_size, y * chunk_size, 0)
-        heights_chunk = get_chunk_heights(x, y, chunk_size, subdivisions, noise_map)
-        biome_influences = get_chunk_biome_influences(x, y, chunk_size, subdivisions, biome_maps)
-        verts, faces, colors = prepare_plane_data(location, chunk_size, subdivisions, heights_chunk, biome_influences)
-        name = f"Plane_{x}_{y}"
-        return (name, verts, faces, colors)
+    plains_biome = np.clip(plains_biome, 0, 0)
+    hills_biome = np.clip(hills_biome, 0, 0)
+    mountains_biome = np.clip(mountains_biome, 1, 1)
+    river = np.clip(river, 0, 0)
+    sand = np.clip(sand, 0, 0)
 
-    # Prepare data in parallel
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        mesh_data_list = list(executor.map(prepare_chunk_data, chunk_coords))
+    biome_maps = {
+        'plains': plains_biome,
+        'hills': hills_biome,
+        'mountains': mountains_biome,
+        'rivers': river,
+        'sand': sand
+    }
 
-    t = timeit(t, "Prepare chunk data")
-
-    # Create meshes
-    for name, verts, faces, colors in mesh_data_list:
-        create_mesh_object(name, verts, faces, colors)
-
-    t = timeit(t, "Create meshes")
+    generate_3d_map(noise_map_before, biome_maps, chunk_size, subdivisions, int_world_size)
 
     # Update the viewport
     update_viewport()
@@ -508,7 +595,8 @@ def get_noise_map(chunk_size, world_size, seed=None, frequency=8, waveLength=128
     y_range = (0, size)
 
     noise_map, _ = npn.perlinGenerator(noise, x_range, y_range)
-    return noise_map
+
+    return np.array(noise_map, dtype=np.float16)
 
 def get_chunk_heights(x_chunk, y_chunk, size, subdivisions, map):
     grid_size = subdivisions + 2
